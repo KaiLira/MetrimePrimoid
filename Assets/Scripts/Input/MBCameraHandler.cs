15bf6b22f8b39c9ccefae0cc962116960a70f070
Kai <kai520958@gmail.com>
2023-01-20 09:07:28 -0600

    Camera avoids colliders

--- Assets/Scripts/Input/MBCameraHandler.cs
+++ Assets/Scripts/Input/MBCameraHandler.cs
@@ -15,25 +15,36 @@ public class MBCameraHandler : MonoBehaviour
     public float m_targetYaw;
     public float m_targetDistance;
     public float m_speed;
-    public float m_sensitivity = 50;
+    public float m_sensitivity = 75;
     private Vector2 m_intention = Vector2.zero;
 
     void FixedUpdate()
     {
         // Modify angles with input
         m_targetYaw -= m_intention.x * m_sensitivity * Time.fixedDeltaTime;
-        m_targetYaw = Mathf.Abs(m_targetYaw) % 360f;
+        m_targetYaw %= 360f;
         m_targetPitch = Mathf.Clamp(
-            m_targetPitch + m_intention.y * m_sensitivity * Time.fixedDeltaTime,
+            m_targetPitch - m_intention.y * m_sensitivity * Time.fixedDeltaTime,
             5,
             80
             );
         m_targetPitch = Mathf.Abs(m_targetPitch) % 360f;
 
-        // Get the target position of the camera
+        // Get the target position for the camera
         Vector3 targetPos = Utils.Vec3FromComps
             (m_targetPitch, m_targetYaw, m_targetDistance)
             + m_morphBall.position;
+
+        // Check for collidiers obstructing view
+        if (Physics.Raycast(
+            m_morphBall.position,
+            (targetPos - m_morphBall.position),
+            out RaycastHit hit,
+            (targetPos - m_morphBall.position).magnitude
+            ))
+        {
+            targetPos = hit.point;
+        }
 
         // Lerp the position of the camera
         transform.position = Vector3.Lerp
