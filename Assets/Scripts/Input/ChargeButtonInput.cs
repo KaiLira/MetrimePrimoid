a0390743cd4c0e2bdaa0c78e37f00907943efe3c
Kai <kai520958@gmail.com>
2023-01-20 09:42:04 -0600

    Charge Input code

---
+++ Assets/Scripts/Input/ChargeButtonInput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Calls unity events with a float between 0 and 1 indicating it's progress
+/// to a "fully charged" input
+/// </summary>
+public class ChargeButtonInput : MonoBehaviour
+{
+    public float m_maxTime;
+    private float m_currentTime = 0;
+    private bool m_isPressed;
+    public UnityEvent<float> m_buttonInput;
+
+    public void ButtonInput(InputAction.CallbackContext context)
+    {
+        m_isPressed = context.ReadValueAsButton();
+        if (!m_isPressed)
+        {
+            m_buttonInput?.Invoke(Mathf.Clamp01(m_maxTime / m_currentTime));
+            m_currentTime = 0;
+        }
+    }
+
+    void Update()
+    {
+        if (m_isPressed)
+            m_currentTime += Time.deltaTime;
+    }
+}
