c7537af5648d6d2a5aedaebb9cfcb96c4ef6f190
mariorojas09 <98665833+mariorojas09@users.noreply.github.com>
2023-01-27 09:13:54 -0600

    Space pirate finishing touches
    *finished pirate attack behaivour

--- Assets/Scripts/Enemy/Attack.cs
+++ Assets/Scripts/Enemy/Attack.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
@@ -8,16 +9,42 @@ public class Attack : State
     [SerializeField] private Detection m_PlayerDetection;
     public Transform m_Muzzle;
     public GameObject m_Bullet;
+    public IEnumerator m_CoroutineInstance;
 
     public override void OnEnter()
     {
-        m_PlayerDetection = gameObject.GetComponent<Detection>();
+        m_CoroutineInstance = CoroutineTime();
+        StartCoroutine(m_CoroutineInstance);
+        m_PlayerDetection = transform.Find("Detection").GetComponent<Detection>();
         m_Muzzle = transform.Find("Muzzle");
-        Resources.Load("");
+        m_Bullet = Resources.Load("Prefabs/Bullet") as GameObject;
+        if (m_Bullet == null)
+        {
+            Debug.Log("coso1");
+        }
     }
 
     public override void OnUpdate()
     {
-        base.OnUpdate();
+        transform.LookAt(m_PlayerDetection.m_Direction);
+        if (m_PlayerDetection.m_IsEnemy == false)
+        {
+            m_StateMachine.SetState(m_StateMachine.m_Patrol);
+        }
+    }
+
+    public override void OnExit()
+    {
+        StopCoroutine(m_CoroutineInstance);
+    }
+
+     IEnumerator CoroutineTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.5f);
+            var bullet = Instantiate(m_Bullet);
+            bullet.transform.SetPositionAndRotation(m_Muzzle.position, m_Muzzle.rotation);
+        }
     }
 }
